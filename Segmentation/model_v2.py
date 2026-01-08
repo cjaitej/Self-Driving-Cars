b3ad6e60b23ead4e58c36bc9e894c04ca05a2b94
Jaitej <76912409+cjaitej@users.noreply.github.com>
2026-01-08 18:20:42 +0530

    Update model_v2.py

--- Segmentation/model_v2.py
+++ Segmentation/model_v2.py
@@ -90,17 +90,10 @@ class IoULoss(nn.Module):
         super(IoULoss, self).__init__()
 
     def forward(self, inputs, targets, smooth=1):
-
-        #comment out if your model contains a sigmoid or equivalent activation layer
         inputs = F.sigmoid(inputs)
-
-
-        #flatten label and prediction tensors
         inputs = inputs.view(-1)
         targets = targets.view(-1)
 
-        #intersection is equivalent to True Positive count
-        #union is the mutually inclusive area of all labels & predictions
         intersection = (inputs * targets).sum()
         total = (inputs + targets).sum()
         union = total - intersection
@@ -132,4 +125,5 @@ class SegmentationLoss(nn.Module):
 # lossFunction = DepthEstimationLoss()
 # img = ToTensor()(plt.imread("D:/Major_Project_Initial/depth/test.png"))
 # loss = lossFunction(img, img)
+
 # print(loss)
